7ac145634839a5401bfd38900ae83359cf498872
Stefan Sabev <stevenbg91@gmail.com>
2024-06-30 18:11:57 +0200

    Add few basic components

---
+++ src/components/HomeSlider.jsx
@@ -0,0 +1,17 @@
+export default function HomeSlider() {
+    return (
+        <div className="home-slider">
+            <div className="main-slider">
+                <div className="main-slider-item">
+                    <img src="img/slider-1.jpg" alt="Slider Image" />
+                </div>
+                <div className="main-slider-item">
+                    <img src="img/slider-2.jpg" alt="Slider Image" />
+                </div>
+                <div className="main-slider-item">
+                    <img src="img/slider-3.jpg" alt="Slider Image" />
+                </div>
+            </div>
+        </div>
+    );
+}
